92018d7bf94bc39b562249440c03d942e9f96184
Den_Evteev <44364287+DenisEvteev@users.noreply.github.com>
2019-05-03 15:58:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+
+#include "My_Assembly.h"
+
+
+int main()
+{
+
+    My_Assembly* compilator = new My_Assembly;
+
+    compilator->Tokenization();
+
+    compilator->Translation();
+
+
+    compilator->Logics();
+
+    compilator->Making_Object_File();
+
+
+    delete compilator;
+
+
+    return 0;
+}
